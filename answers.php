e50b29650ac87c6569ed6017ea3f90627d58b49d
Christian Reotutar <32468132+creotutar@users.noreply.github.com>
2020-12-11 11:39:09 -0500

    answers.php: Change version in code as well (#12)
    The version must be changed in the code as well.
    
    J=SLAP-934
    TEST=manual

--- answers.php
+++ answers.php
@@ -3,7 +3,7 @@
  * @wordpress-plugin
  * Plugin Name:   Yext Answers
  * Description:   Your customers have questions. Can your website answer them? The Yext Answers plugin allows you to seamlessly integrate your Yext Answers search bar and search results page with your website.
- * Version:       1.0.0
+ * Version:       1.0.1
  * Author:        Yext Engineering
  * Author URI:    https://www.yext.com
  * Text Domain:   yext-answers-plugin
@@ -17,7 +17,7 @@ if (!defined('WPINC')) {
 /**
  * Current plugin version.
  */
-define('YEXT_ANSWERS_PLUGIN_VERSION', '1.0.0');
+define('YEXT_ANSWERS_PLUGIN_VERSION', '1.0.1');
 
 /**
  * Gets the value for a given settings option for the Yext Answers plugin
